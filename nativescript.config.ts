b26a8e26090dfb372a765153deb82356bfd010ac
Osei Fortune <fortune.osei@yahoo.com>
2026-04-30 09:07:12 -0400

    chore(android): enable v8 fast api

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -4,8 +4,9 @@ export default {
   id: 'org.nativescript.NativeScriptStarterKitRacing',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
+  discardUncaughtJsExceptions: true,
   android: {
-    v8Flags: '--expose_gc',
+    v8Flags: '--expose_gc  --allow-natives-syntax --turbo-fast-api-calls',
     markingMode: 'none'
   }
 } as NativeScriptConfig;
